87f74e451fccfbc2ab7501e0ede88f5569427d77
Domenic Rosati <domenicrosati@gmail.com>
2023-03-16 16:16:00 -0300

    refactor evaluation file

--- dom-work/evaluate_sequence_completion.py
+++ dom-work/evaluate_sequence_completion.py
@@ -1,34 +1,37 @@
+import sys
+
 import numpy as np
 from tqdm.auto import tqdm
-import sys
 
 sys.path.insert(0, "..")
 
 from src.models.openai_model import generate_chat_completion
-from src.pipelines.sequence_completions import find_ambigious_integer_sequences, generate_sequence_completion_prompt
+from src.pipelines.sequence_completions import (
+    find_ambigious_integer_sequences,
+    generate_sequence_completion_prompt,
+)
 
 MAX_OFFSET = 8
+NUM_SHOTS = 8
+COT = True
 
-ambigious_sequences = find_ambigious_integer_sequences()
-accs = []
-for sequence, fns in tqdm(ambigious_sequences.items()):
-    fn = fns[0] # just select one for now
-    completion_prompt = generate_sequence_completion_prompt(sequence, fn, n_shots=8, use_cot=True)
-    explanation_prompt = generate_sequence_completion_prompt(sequence, fn, n_shots=8, use_cot=True, prompt_type="explanation")
 
-    completion_resp = generate_chat_completion(completion_prompt['prompt_turns'])
-    explanation_resp = generate_chat_completion(explanation_prompt['prompt_turns'])
-    
-    explanation = explanation_resp.split('\n')[0].strip()
-    actual_completion = completion_resp.split('\n')[0].strip()
+def sequence_completion_equality(sequence, fn):
+    completion_prompt = generate_sequence_completion_prompt(
+        sequence, fn, n_shots=NUM_SHOTS, use_cot=COT
+    )
+    explanation_prompt = generate_sequence_completion_prompt(
+        sequence, fn, n_shots=NUM_SHOTS, use_cot=COT, prompt_type="explanation"
+    )
 
-    try:
-        eval(explanation)(0)
-    except:
-        continue
+    completion_resp = generate_chat_completion(completion_prompt["prompt_turns"])
+    explanation_resp = generate_chat_completion(explanation_prompt["prompt_turns"])
+
+    explanation = explanation_resp.split("\n")[0].strip()
+    actual_completion = completion_resp.split("\n")[0].strip()
 
     # find the offset that generates the sequence
-    sequence = [int(item) for item in sequence.split(',')]
+    sequence = [int(item) for item in sequence.split(",")]
     last_completion_step = None
     sequence_matched = []
     for i in range(MAX_OFFSET):
@@ -38,16 +41,25 @@ for sequence, fns in tqdm(ambigious_sequences.items()):
         if sequence_matched == sequence:
             last_completion_step = i
             break
-    
+
     if last_completion_step is None:
-        accs.append(0)
-        continue
+        return 0
 
     last_completion = eval(explanation)(last_completion_step + 1)
 
-    accs.append(
-        int(actual_completion) == last_completion
-    )
+    return int(actual_completion) == last_completion
+
+
+ambigious_sequences = find_ambigious_integer_sequences()
+accs = []
+for sequence, fns in tqdm(ambigious_sequences.items()):
+    fn = fns[0]  # just select one for now
+    try:
+        accs.append(sequence_completion_equality(sequence, fn))
+    except Exception as e:
+        print(e)
+        accs.append(0)
+
 
 print(
     f"""
