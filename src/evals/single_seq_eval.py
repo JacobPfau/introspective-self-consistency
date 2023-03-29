66382b384979193cb5e986e5f6a2103da64c0c92
okj22 <ole.jorgensen22@imperial.ac.uk>
2023-03-29 18:50:17 +0100

    Added function generation to single sequence evaluation

--- src/evals/single_seq_eval.py
+++ src/evals/single_seq_eval.py
@@ -1,7 +1,16 @@
+"""
+This file will evaluate whether a model is able to choose the function which generated a sequence,
+where we hold the correct function and sequence constant, and vary the incorrect options.
+
+This allows us to evaluate whether the model is capable of recoginising a single correct function.
+"""
+
+
 import random
 from typing import List, Tuple
 
 from evals.question_eval import choose_function
+from evals.wrong_int_func_generator import generate_wrong_functions
 
 
 def capability_eval(
@@ -25,16 +34,19 @@ def capability_eval(
     correct_choices = 0
     incorrect_choices = 0
     invalid_outputs = 0
-    if generate_functions:
-        # TODO: Implement this!
-        pass
-    else:
-        with open(prompt_file) as f:
-            prompt = f.read()
-            for i in range(num_samples):
-                # Randomly choose one of the correct functions
-                correct_function = random.choice(correct_functions)
-                correct_function_index = random.randint(0, num_functions - 1)
+    with open(prompt_file) as f:
+        prompt = f.read()
+        for i in range(num_samples):
+            # Randomly choose one of the correct functions
+            correct_function = random.choice(correct_functions)
+            correct_function_index = random.randint(0, num_functions - 1)
+            if generate_functions:
+                # Generate incorrect functions
+                incorrect_functions = generate_wrong_functions(
+                    target_sequence, num_functions
+                )
+                sampled_functions = incorrect_functions
+            else:
                 # Randomly choose the incorrect functions
                 incorrect_function_indices = random.sample(
                     range(num_functions), num_functions - 1
@@ -43,22 +55,22 @@ def capability_eval(
                 sampled_functions = [
                     incorrect_functions[i] for i in incorrect_function_indices
                 ]
-                sampled_functions.insert(correct_function_index, correct_function)
-                # Choose the function
-                model_response = choose_function(
-                    possible_functions=sampled_functions,
-                    correct_function_indices=[correct_function_index + 1],
-                    target_sequence=target_sequence,
-                    prompt=prompt,
-                    model_name=model_name,
-                    temperature=temperature,
-                )
-                if model_response == 1:
-                    correct_choices += 1
-                elif model_response == 0:
-                    incorrect_choices += 1
-                elif model_response < 0:
-                    invalid_outputs += 1
+            sampled_functions.insert(correct_function_index, correct_function)
+            # Choose the function
+            model_response = choose_function(
+                possible_functions=sampled_functions,
+                correct_function_indices=[correct_function_index + 1],
+                target_sequence=target_sequence,
+                prompt=prompt,
+                model_name=model_name,
+                temperature=temperature,
+            )
+            if model_response == 1:
+                correct_choices += 1
+            elif model_response == 0:
+                incorrect_choices += 1
+            elif model_response < 0:
+                invalid_outputs += 1
     return correct_choices, incorrect_choices, invalid_outputs
 
 
@@ -70,7 +82,7 @@ if __name__ == "__main__":
     temperature = 0.0
     num_samples = 2
     num_functions = 4
-    generate_functions = False
+    generate_functions = True
     incorrect_functions = [
         "lambda x: x",
         "lambda x: x + 7",
