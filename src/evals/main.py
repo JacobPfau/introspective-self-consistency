24b1077b3fb1287ebd3659c6701b498c1577547d
okj22 <ole.jorgensen22@imperial.ac.uk>
2023-04-14 13:26:31 +0100

    Added davinci final run

--- src/evals/main.py
+++ src/evals/main.py
@@ -46,7 +46,7 @@ parser.add_argument(
 parser.add_argument("--on-ambiguous-sequences", default="True", type=str2bool)
 parser.add_argument(
     "--model",
-    default="CHAT",
+    default="DAVINCI",
     type=str,
     choices=["CHAT", "DAVINCI"],
 )
