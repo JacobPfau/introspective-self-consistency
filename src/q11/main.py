146bb71afdc921b2c57210f2ab1045c8c17d194e
okj22 <ole.jorgensen22@imperial.ac.uk>
2023-05-11 15:51:14 +0100

    Added experiments using binary sequences

--- src/q11/main.py
+++ src/q11/main.py
@@ -26,7 +26,7 @@ def str2bool(v):
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--sequence-type",
-    default="integer",
+    default="binary",
     type=str,
     choices=["binary", "integer"],
 )
@@ -34,11 +34,11 @@ parser.add_argument(
 parser.add_argument("--on-ambiguous-sequences", default="True", type=str2bool)
 parser.add_argument(
     "--model",
-    default="DAVINCI",
+    default="CHAT",
     type=str,
     choices=["CHAT", "DAVINCI"],
 )
-parser.add_argument("--num-shots", default=16, type=int)
+parser.add_argument("--num-shots", default=4, type=int)
 parser.add_argument("--use-cot", default=False, type=str2bool, nargs="?", const=True)
 parser.add_argument("--num-samples", default=1, type=int)
 
@@ -49,50 +49,53 @@ if __name__ == "__main__":
     sequence_functions = None
     if args.on_ambiguous_sequences:
         if args.sequence_type == "integer":
-            sequence_functions = all_sequence_functions
-            # Get the ambiguous sequences
-            # Use default parameters for now
-            results = {}
-            ambiguous_sequences = find_ambiguous_integer_sequences()
-            for sequence in ambiguous_sequences:
-                # turn the sequence from a string into a list of integers
-                int_sequence = [int(x) for x in sequence.split(",")]
-                total += 1
-                print("Total: ", total)
-                print(f"Sequence: {sequence}")
-                for _ in range(2):
-                    try:
-                        (
-                            consistent_explanations,
-                            inconsistent_explanations,
-                            invalid_explanations,
-                        ) = self_consistency_evaluation(
-                            model_name=args.model,
-                            sequence=int_sequence,
-                            distribution="default",
-                            base=10,
-                            shots=args.num_shots,
-                            shot_method="random",
-                            temperature=0.0,
-                            samples=args.num_samples,
-                        )
-                    except Exception as e:
-                        print("oopies")
-                        print(e)
+            base = 10
+        elif args.sequence_type == "binary":
+            base = 2
+        else:
+            raise ValueError("Unknown sequence type.")
+        sequence_functions = all_sequence_functions
+        # Get the ambiguous sequences
+        # Use default parameters for now
+        results = {}
+        ambiguous_sequences = find_ambiguous_integer_sequences()
+        for sequence in ambiguous_sequences:
+            # turn the sequence from a string into a list of integers
+            int_sequence = [int(x) for x in sequence.split(",")]
+            total += 1
+            print("Total: ", total)
+            print(f"Sequence: {sequence}")
+            for _ in range(2):
+                try:
+                    (
+                        consistent_explanations,
+                        inconsistent_explanations,
+                        invalid_explanations,
+                    ) = self_consistency_evaluation(
+                        model_name=args.model,
+                        sequence=int_sequence,
+                        distribution="default",
+                        base=base,
+                        shots=args.num_shots,
+                        shot_method="random",
+                        temperature=0.0,
+                        samples=args.num_samples,
+                    )
+                except Exception as e:
+                    print("oopies")
+                    print(e)
+                else:
+                    if sequence in results:
+                        results[sequence]["consistent"] += consistent_explanations
+                        results[sequence]["inconsistent"] += inconsistent_explanations
+                        results[sequence]["invalid"] += invalid_explanations
                     else:
-                        if sequence in results:
-                            results[sequence]["consistent"] += consistent_explanations
-                            results[sequence][
-                                "inconsistent"
-                            ] += inconsistent_explanations
-                            results[sequence]["invalid"] += invalid_explanations
-                        else:
-                            results[sequence] = {
-                                "consistent": consistent_explanations,
-                                "inconsistent": inconsistent_explanations,
-                                "invalid": invalid_explanations,
-                            }
-                        break
+                        results[sequence] = {
+                            "consistent": consistent_explanations,
+                            "inconsistent": inconsistent_explanations,
+                            "invalid": invalid_explanations,
+                        }
+                    break
         else:
             pass
             # TODO: have support for general base sequences here
