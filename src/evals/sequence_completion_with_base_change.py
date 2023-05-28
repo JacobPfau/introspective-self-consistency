376cb64ecc20f6277610f75332c2db18d4f41c3c
ojorgensen <ojorgensen1417@gmail.com>
2023-05-28 20:51:11 +0100

    Fixed hydra issue, results now stored properly

--- src/evals/sequence_completion_with_base_change.py
+++ src/evals/sequence_completion_with_base_change.py
@@ -1,9 +1,14 @@
+import json
+import logging
+
 from src.evals.check_self_consistency import self_consistency_evaluation
 from src.pipelines.sequence_completions import (
     find_ambiguous_integer_sequences,
     sequence_functions,
 )
 from src.utils import reformat_self_consistency_results
+
+logger = logging.getLogger(__name__)
 
 
 def evaluate_compute_dependence_with_base_changes(
@@ -38,11 +43,11 @@ def evaluate_compute_dependence_with_base_changes(
             # turn the sequence from a string into a list of integers
             int_sequence = [int(x) for x in sequence.split(",")]
             total += 1
-            print("Total: ", total)
-            print(f"Sequence: {sequence}")
+            logger.info("Total: ", total)
+            logger.info(f"Sequence: {sequence}")
             for _ in range(2):
                 try:
-                    print("base be: ", base)
+                    logger.info("base be: ", base)
                     (
                         correct_consistent_explanations,
                         correct_inconsistent_explanations,
@@ -60,8 +65,8 @@ def evaluate_compute_dependence_with_base_changes(
                         samples=num_samples,
                     )
                 except Exception as e:
-                    print("oopies")
-                    print(e)
+                    logger.info("oopies")
+                    logger.info(e)
                 else:
                     if sequence in results:
                         results[sequence][
@@ -90,33 +95,13 @@ def evaluate_compute_dependence_with_base_changes(
             pass
             # TODO: have support for general base sequences here
 
-    print(total)
+    logger.info(total)
 
     # Reformat results
     results = reformat_self_consistency_results(results)
 
     # Save the results
-    import datetime
-    import json
-    import os
-
-    now = datetime.datetime.now()
-    now_str = now.strftime("%Y-%m-%d-%H-%M-%S")
-    results_dir = os.path.join(
-        "../results/q1.1",
-        f"{now_str}",
-    )
-    if not os.path.exists(results_dir):
-        os.makedirs(results_dir)
-    results_path = os.path.join(results_dir, "results.json")
-    with open(results_path, "w") as f:
+    with open("results.json", "w") as f:
         json.dump(results, f)
 
-    # Save command line arguments
-    # args_path = os.path.join(results_dir, "args.json")
-    # args_dict = vars(args)
-    # args_dict["sequence_functions"] = all_sequence_functions
-    # with open(args_path, "w") as f:
-    #     json.dump(args_dict, f)
-
-    print(f"Results saved to {results_path}")
+    logger.info("Results saved to results.json")
